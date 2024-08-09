e771c8b2784eac0d586794423060e54ff28bb2f6
harsh-kanjar <harshlankar07@gmail.com>
2024-08-09 23:34:44 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1 className="text-danger">Hello world</h1>  
+    </>
+  )
+}
+
+export default App
